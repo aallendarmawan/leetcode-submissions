43d27e5048e5f08d2d68fb73f89634e8726bc3b4
Allen Darmawan <allenticular@gmail.com>
2026-04-05 23:16:35 +0200

    feat: 657.cpp

---
+++ others/657-robot_return_origin/657.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (char c : moves) {
+            if (c == 'U') ++y;
+            if (c == 'D') --y;
+            if (c == 'L') --x;
+            if (c == 'R') ++x;
+        }
+        return (x == 0 && y == 0);
+    }
+};
